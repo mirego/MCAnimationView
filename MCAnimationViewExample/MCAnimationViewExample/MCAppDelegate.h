f94ab3429c1caecf5203fcfcb6395b6f54f16416
Francois Lambert <flambert@mirego.com>
2013-11-07 17:07:54 -0500

    Add CI and Coverage

---
+++ MCAnimationViewExample/MCAnimationViewExample/MCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MCAppDelegate.h
+//  MCAnimationViewExample
+//
+//  Created by Francois Lambert on 11/7/2013.
+//  Copyright (c) 2013 Mirego Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
